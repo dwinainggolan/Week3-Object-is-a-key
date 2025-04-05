f4a1aa088f6fd780352c8ac7cb7a1a0d553d080c
dwinainggolan <dwigolan@gmail.com>
2025-04-05 15:36:33 +0700

    tugas 2 part 2

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,23 @@
+//DILARANG MENGGUNAKAN METHOD SORT, PELAJARI ALGORITMA SORTING YANG ADA DI GOOGLE
+//saran sih pake bubblesort walau tidak efisien tapi bagus buat belajar sorting
+function urutkanAbjad(str) {
+    // you can only write you
+    // for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      // Tukar posisi
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+
+  
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
